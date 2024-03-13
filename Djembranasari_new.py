564d0e7965a61d95bae4637c737499d5e4f90d81
Rayendraarya26 <88335755+Rayendraarya26@users.noreply.github.com>
2024-03-13 21:27:00 +0700

    test mesh mebur

--- Djembranasari_new.py
+++ Djembranasari_new.py
@@ -34,3 +34,12 @@ def executeCommands():
 	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Cube']]
 	bpy.context.view_layer.objects.active = bpy.data.objects['Cube']
 	bpy.ops.transform.translate(value=(0, 0, 4.82417), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False, alt_navigation=True)
+	bpy.ops.outliner.item_activate(deselect_all=True)
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Cube']]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Cube']
+	bpy.ops.mesh.primitive_uv_sphere_add(radius=1, enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Sphere']]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Sphere']
+	bpy.ops.transform.translate(value=(0, 0, 5.0406), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False, alt_navigation=True)
