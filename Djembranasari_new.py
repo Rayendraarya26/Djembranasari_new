7b64bca910778c722e0b2ffa4e8be44a83066916
Rayendraarya26 <88335755+Rayendraarya26@users.noreply.github.com>
2024-03-13 21:04:23 +0700

    Mindah base mesh villa

--- Djembranasari_new.py
+++ Djembranasari_new.py
@@ -2,3 +2,11 @@ import bpy
 
 def executeCommands():
 	pass
+	bpy.ops.view3d.pastebuffer()
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Omah Jimbaran']]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Omah Jimbaran']
+	bpy.ops.object.editmode_toggle()
+	bpy.ops.object.editmode_toggle()
+	bpy.ops.object.editmode_toggle()
+	bpy.ops.object.editmode_toggle()
